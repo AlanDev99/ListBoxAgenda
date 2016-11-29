e1bf8a4812749eca191df49caebbcc689413ea9a
Alan Viñals <vinals.alan@gmail.com>
2016-11-29 12:22:17 -0300

    Agenda volatil con listBox

--- ListBoxAgenda/Form1.Designer.vb
+++ ListBoxAgenda/Form1.Designer.vb
@@ -22,9 +22,150 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBoxAgenda = New System.Windows.Forms.ListBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnVer = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBoxAgenda
+        '
+        Me.ListBoxAgenda.FormattingEnabled = True
+        Me.ListBoxAgenda.Location = New System.Drawing.Point(231, 25)
+        Me.ListBoxAgenda.Name = "ListBoxAgenda"
+        Me.ListBoxAgenda.Size = New System.Drawing.Size(178, 303)
+        Me.ListBoxAgenda.TabIndex = 0
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(13, 43)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(193, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(13, 106)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(193, 20)
+        Me.txtTelefono.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(13, 162)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(193, 20)
+        Me.txtEmail.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 141)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Email"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Teléfono"
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgregar.Location = New System.Drawing.Point(26, 197)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(161, 30)
+        Me.btnAgregar.TabIndex = 7
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEliminar.Location = New System.Drawing.Point(26, 269)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(161, 30)
+        Me.btnEliminar.TabIndex = 8
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnVer
+        '
+        Me.btnVer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVer.Location = New System.Drawing.Point(26, 233)
+        Me.btnVer.Name = "btnVer"
+        Me.btnVer.Size = New System.Drawing.Size(161, 30)
+        Me.btnVer.TabIndex = 9
+        Me.btnVer.Text = "Ver"
+        Me.btnVer.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(26, 305)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(161, 30)
+        Me.btnSalir.TabIndex = 10
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 349)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnVer)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.ListBoxAgenda)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ListBoxAgenda As ListBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnVer As Button
+    Friend WithEvents btnSalir As Button
 End Class
--- ListBoxAgenda/Form1.vb
+++ ListBoxAgenda/Form1.vb
@@ -1,3 +1,82 @@
 ﻿Public Class Form1
+    'Cantidad de registros acumulados
+    Dim totalRegistros As Integer
 
+    'Cantidad maxima de registros + 1
+    Dim cantContactos As Integer
+
+    Structure Contacto
+        Dim nombre As String
+        Dim telefono As String
+        Dim email As String
+    End Structure
+
+    Dim MiAgenda() As Contacto
+    'Definimos el array MiAgenda
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Este procedimiento es invocado automaticamente al iniciar el formulario
+        totalRegistros = 0
+        reservarAgenda()
+
+    End Sub
+
+    Sub reservarAgenda()
+        Dim Message As String = "¿Cuantos contactos va a ingresar?"
+        Me.cantContactos = Val(InputBox(Message, "Ingresar", "5")) - 1
+        'ReDimension de la agenda de 0 a cantContactos
+        ReDim MiAgenda(cantContactos)
+        'Reservado (cantContactos) + 1 espacios
+        'Por ese motivo resto -1 en el InputBox
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        'Verificacion del tamaño del array
+        'MsgBox(MiAgenda.Count)
+        If totalRegistros > cantContactos Then
+            MsgBox("Lista completa", 16, "Error")
+        Else
+            MiAgenda(totalRegistros).nombre = txtNombre.Text
+            MiAgenda(totalRegistros).telefono = txtTelefono.Text
+            MiAgenda(totalRegistros).email = txtEmail.Text
+
+            ListBoxAgenda.Items.Add(txtNombre.Text)
+
+            txtNombre.Clear()
+            txtTelefono.Clear()
+            txtEmail.Clear()
+            txtNombre.Focus()
+
+            totalRegistros += 1
+        End If
+
+    End Sub
+
+    Private Sub ListBoxAgenda_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxAgenda.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim n As Integer = ListBoxAgenda.SelectedIndex
+        ListBoxAgenda.Items.RemoveAt(n)
+
+        For i = n To totalRegistros - 2
+            MiAgenda(i).nombre = MiAgenda(i + 1).nombre
+            MiAgenda(i).telefono = MiAgenda(i + 1).telefono
+            MiAgenda(i).email = MiAgenda(i + 1).email
+        Next i
+
+        totalRegistros -= 1
+    End Sub
+
+    Private Sub btnVer_Click(sender As Object, e As EventArgs) Handles btnVer.Click
+        Dim n As Integer = ListBoxAgenda.SelectedIndex
+        txtNombre.Text = MiAgenda(n).nombre
+        txtTelefono.Text = MiAgenda(n).telefono
+        txtEmail.Text = MiAgenda(n).email
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
 End Class
